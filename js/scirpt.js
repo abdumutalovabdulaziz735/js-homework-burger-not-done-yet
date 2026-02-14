4699abaf9f41cbd8aff392696bf4c835ff899c79
Abdulaziz Abdumutalov <abdumutalovabdulaziz735.com>
2026-02-14 23:16:56 +0500

    first commit

---
+++ js/scirpt.js
@@ -0,0 +1,39 @@
+
+
+const numbers =document.querySelector('.number-100'),
+    span= document.querySelector('.raise');
+    let hello = 0
+
+
+function item() {
+    span.innerHTML = hello
+    if (hello < 100) {
+        hello++
+        
+    }
+    if(hello == 20){
+    numbers.style = `color: red`
+        span.style = `color: red`
+    }
+     if(hello == 40){
+    numbers.style = `color: yellow`
+        span.style = `color: yellow`
+    }
+     if(hello == 60){
+    numbers.style = `color: green`
+        span.style = `color: green`
+    }
+    if(hello == 80){
+    numbers.style = `color: aqua`
+        span.style = `color: aqua`
+    }
+    if(hello == 100){
+    numbers.style = `color: black`
+        span.style = `color: black`
+    }
+
+    setTimeout(() => {
+        item()
+    }, 100);
+}
+item()
